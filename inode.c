f846958c9b48d226467daa7ba63e48c4a5896a37
sahinonur <sahin@bu.edu>
2015-04-30 15:39:56 +0800

    add_entry for the inode

--- inode.c
+++ inode.c
@@ -41,6 +41,36 @@ inode_t* inode_get_pointer(uint16_t index){
     return &inode_head[index];
 }
 
+uint16_t inode_add_entry(uint16_t parent_inode, char* name, uint16_t isFile){
+ 
+  inode_t* parent_ptr = inode_get_pointer(parent_inode);
+ 
+  // Create a new child inode
+  inode_t child_node;
+  uint16_t child_node_index = inode_get_free_index();
+  if(child_node_index==0) return 0; // Could not allocate inode
+  child_node.size=0;
+  (isFile==1) ? (strcpy(child_node.type,"reg")):(strcpy(child_node.type,"dir"));
+  
+  // Create a new directory
+  directory_entry_t new_dir;
+  strcpy(new_dir.filename,name);
+  new_dir.index_node_number = child_node_index;
+  
+  // Find a target block for adding a new entry
+  uint8_t block_offset = (parent_ptr->size)/(BLOCK_SIZE); // index into the location table
+  uint8_t in_block_offset = (parent_ptr->size % BLOCK_SIZE)/(sizeof(directory_entry_t)); // index into the block
+  block_t* target_block = inode_get_block(parent_inode, block_offset);
+  
+  if(target_block==NULL) return 0; // Could not a allocate block
+  
+  // Add newly created entry to the parent node
+  target_block[in_block_offset] = new_dir;
+  parent_ptr->size += sizeof(directory_entry_t);
+  
+  return child_node_index;
+}
+
 /*
 uint16_t inode_add_directory_entry(uint16_t inode_index, char* dirName){
   inode_t* parent_node = inode_get_pointer(inode_parent_index);
