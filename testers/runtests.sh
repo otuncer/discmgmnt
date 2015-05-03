3208a21d1e38f22eceb3e99c780d1b5ffaf1cfef
otuncer <ozan.tuncer.1@gmail.com>
2015-05-02 20:25:50 -0400

    added unit test TODOs

--- testers/runtests.sh
+++ testers/runtests.sh
@@ -19,3 +19,9 @@ rm a.out
 gcc -DDEBUG ${base}testers/inode_test.c ${base}inode.c ${base}block.c
 ./a.out
 rm a.out
+
+# TODO inode add entry test
+
+# TODO inode find entry test
+
+# TODO remove entry test
