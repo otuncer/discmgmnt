d09937defc7aa06f97c6fb7e18ee96bc461ae8eb
otuncer <ozan.tuncer.1@gmail.com>
2015-05-05 14:17:39 +0800

    Cleaned barebones ramdisk module

--- ramdisk_module.c
+++ ramdisk_module.c
@@ -10,45 +10,49 @@
 #include <linux/wait.h>
 #include <linux/vmalloc.h>
 
+#include "constants.h"
 #include "file_operations.h"
-
-#define KILOBYTES 1024
-#define MEGABYTES (KILOBYTES*1024)
 
 MODULE_LICENSE("GPL");
 
-/************************/
 /** PROCFS GLOBAL VARS **/
-/************************/
-static struct file_operations pseudo_dev_proc_operations;
 static struct proc_dir_entry *proc_entry;
+static struct file_operations pseudo_dev_proc_operations;
+
+/** PROCFS FUNCTIONS **/
+static int pseudo_device_ioctl(struct inode *inode, struct file *file,
+			       unsigned int cmd, unsigned long arg);
+
+/***** RAMDISK GLOBAL VARS ******/
 
 char* ramdisk_mem;
 inode_t* inode_head;
 uint8_t* block_bitmap;
 block_t* blocks;
 
-/************************/
 /***** INIT MODULE ******/
-/************************/
 static int __init initialization_routine(void) {
   printk("<1> Loading RAMDISK\n");
 
   /* Create RAMDISK /proc entry */
   proc_entry = create_proc_entry("ramdisk", 0444, NULL);
   if(!proc_entry){
-      printk("<1> Error creating /proc entry.\n");
-      return -ENOMEM;
+    printk("<1> Error creating /proc entry.\n");
+    return -ENOMEM;
   }
+  pseudo_dev_proc_operations.ioctl = pseudo_device_ioctl;
 
 	/* Allocate RAMDISK memory area */
-	ramdisk_mem = vmalloc(2*MEGABYTES);
+	if( (ramdisk_mem = vmalloc(PARTITION_SIZE)) == NULL){
+    return -ENOMEM;
+  }
 
   proc_entry->proc_fops = &pseudo_dev_proc_operations;
 	
   return 0;
 }
 
+/***** REMOVE MODULE ******/
 static void __exit cleanup_routine(void) {
   
   remove_proc_entry("ramdisk", NULL);
@@ -60,6 +64,24 @@ static void __exit cleanup_routine(void) {
   printk("<1> RAMDISK unloded\n");
   return;
 }
+
+static int pseudo_device_ioctl(struct inode *inode, struct file *file,
+                               unsigned int cmd, unsigned long arg)
+{
+  switch (cmd){
+
+  //case IOCTL_KEYBOARD:
+  
+  
+  //  break;
+  default:
+    return -EINVAL;
+    break;
+  }
+  
+  return 0;
+}
+
 
 module_init(initialization_routine);
 module_exit(cleanup_routine);
